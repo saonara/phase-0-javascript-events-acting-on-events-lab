4362fdae468dfd99d576a142949fcfad76888cb5
odongo <saonarawestinc@outlook.com>
2024-03-17 20:27:46 +0300

    Completed Moving_things_with_js_by_acting_on_events lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Your code here
+const dodger = document.querySelector("#dodger");
+
+// document.addEventListener("keydown", (e) => {
+//   if (e.key === "ArrowRight") {
+//     let currentLeft = parseInt(dodger.style.left.replace("px", ""), 10);
+//     dodger.style.left = `${currentLeft + 1}px`;
+//   }
+
+//   if (e.key === "ArrowLeft") {
+//     let currentLeft = parseInt(dodger.style.left.replace("px", ""), 10);
+//     dodger.style.left = `${currentLeft - 1}px`;
+//   }
+// });
+
+function moveDodgerRight() {
+  let currentLeft = parseInt(dodger.style.left.replace("px", ""), 10);
+  dodger.style.left = `${currentLeft + 1}px`;
+}
+
+function moveDodgerLeft() {
+  let currentLeft = parseInt(dodger.style.left.replace("px", ""), 10);
+  dodger.style.left = `${currentLeft - 1}px`;
+}
