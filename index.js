95cce7e1f0b2cd9c51fea7af01beed4ff53c1fc4
odongo <saonarawestinc@outlook.com>
2024-04-03 15:49:10 +0300

    Completed actiing-on-events lab

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const dodger = document.querySelector("#dodger");
 
-// document.addEventListener("keydown", (e) => {
-//   if (e.key === "ArrowRight") {
-//     let currentLeft = parseInt(dodger.style.left.replace("px", ""), 10);
-//     dodger.style.left = `${currentLeft + 1}px`;
-//   }
+document.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
 
-//   if (e.key === "ArrowLeft") {
-//     let currentLeft = parseInt(dodger.style.left.replace("px", ""), 10);
-//     dodger.style.left = `${currentLeft - 1}px`;
-//   }
-// });
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
 
 function moveDodgerRight() {
   let currentLeft = parseInt(dodger.style.left.replace("px", ""), 10);
